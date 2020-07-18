807d29f3f80adcf028714b64d31f32239a08b4b4
Constikri <Constantin@krischkeonline.de>
2020-07-19 00:53:38 +0200

    save point

--- matrix_mult_mpi/src/matrix_mult_mpi.cpp
+++ matrix_mult_mpi/src/matrix_mult_mpi.cpp
@@ -108,24 +108,6 @@ void multiply(double **A, double **B, double **result, int x, int y){
 	}
 }
 
-void multiply_part(double **A, double **B, double **result, int x_a,int x_b, int y, int offset, int rank){
-	for(int i = 0; i < x_a; i++){
-		int of_run = offset;
-		for(int j = 0; j < x_b; j++){
-//			if(rank == 3 && of_run == 1){
-//				cout << "prev res: " << result[i][of_run];
-//			}
-			for(int k = 0; k < y; k++){
-				result[i][of_run] += A[i][k] * B[j][k];
-			}
-			if(rank == 3){
-				cout << "res: " << result[i][of_run] << endl;
-			}
-			of_run++;
-		}
-	}
-}
-
 int main(int argc, char *argv[]) {
 
 	MPI::Init(argc, argv);
@@ -133,15 +115,16 @@ int main(int argc, char *argv[]) {
 	int rank = MPI::COMM_WORLD.Get_rank();
 
 	double **A, **B, **result, **recvA, **recvB;
-	int **sendcnt, **displ, **recv_dimensions;
-	int i;
+	int **recv_dimensions;
+	int *sendcnt, *displ;
+	int counter;
 
-	sendcnt = alloc_2d_array_int(2, size);
-	displ = alloc_2d_array_int(2, size);
-	recv_dimensions = alloc_2d_array_int(3, size);
+	sendcnt = (int *)malloc(size*sizeof(int));
+	displ = (int *)malloc(size*sizeof(int));
+	recv_dimensions = alloc_2d_array_int(2, size);
 
 	if(rank == 0){
-		int xdim, ydim, xportion, yportion, xrest, yrest;
+		int xdim, ydim, xportion, xrest;
 		cout << "Please put in the x dimension of the first matrix:" << endl;
 		cin >> xdim;
 		cout << "Please put in the y dimension of the first matrix:" << endl;
@@ -152,7 +135,7 @@ int main(int argc, char *argv[]) {
 		result = alloc_2d_array_double(xdim, xdim);
 
 		generate(A, xdim, ydim);
-		generate(B, ydim, xdim);
+		generate(B, xdim, ydim);
 		multiply(A, B, result, xdim, ydim);
 
 		char filename[15] = {0};
@@ -175,93 +158,115 @@ int main(int argc, char *argv[]) {
 		}
 		print_matrix_to_file(result, filename, xdim, xdim, 0);
 
+		free(result[0]);
+		free(result);
+
 		xportion = xdim/(size);
 		xrest = xdim%(size);
-		yportion = ydim/(size);
-		yrest = ydim%(size);
 
-		recv_dimensions[2][0] = xdim;
-		recv_dimensions[2][1] = ydim;
+		recv_dimensions[1][0] = xdim;
+		recv_dimensions[1][1] = ydim;
 
-		int sumA = 0;
-		int sumB = 0;
-		for(i = 0; i < size; i++){
-			sendcnt[0][i] = xportion*ydim;
-			sendcnt[1][i] = yportion*xdim;
-			recv_dimensions[0][i] = xportion;
-			recv_dimensions[1][i] = yportion;
+		int sum = 0;
+		for(counter = 0; counter < size; counter++){
+			sendcnt[counter] = xportion*ydim;
+			recv_dimensions[0][counter] = xportion;
 			if(xrest > 0){
-				sendcnt[0][i] += ydim;
-				recv_dimensions[0][i] += 1;
+				sendcnt[counter] += ydim;
+				recv_dimensions[counter] += 1;
 				xrest--;
 			}
-			if(yrest > 0){
-				sendcnt[1][i] += xdim;
-				recv_dimensions[1][i] += 1;
-				yrest--;
-			}
-			displ[0][i] = sumA;
-			sumA += sendcnt[0][i];
-			displ[1][i] = sumB;
-			sumB += sendcnt[1][i];
+			displ[counter] = sum;
+			sum += sendcnt[counter];
 		}
 	}
 
-	MPI::COMM_WORLD.Bcast(&recv_dimensions[0][0], 3*size, MPI_INT, 0);
-	MPI::COMM_WORLD.Bcast(&sendcnt[0][0], 2*size, MPI_INT, 0);
-	MPI::COMM_WORLD.Bcast(&displ[0][0], 2*size, MPI_INT, 0);
+	MPI::COMM_WORLD.Bcast(&recv_dimensions[0][0], 2*size, MPI_INT, 0);
+	MPI::COMM_WORLD.Bcast(&sendcnt[0], size, MPI_INT, 0);
+	MPI::COMM_WORLD.Bcast(&displ[0], size, MPI_INT, 0);
 
-	recvA = alloc_2d_array_double(recv_dimensions[0][rank], recv_dimensions[2][1]);
-	recvB = alloc_2d_array_double(recv_dimensions[1][rank], recv_dimensions[2][1]);
+//	if(rank == 3){
+//		cout << "dimensions" << endl;
+//		print_matrix_int(recv_dimensions, 2, size, 0);
+//		cout << "sendcnt" << endl;
+//		cout << sendcnt[0] << " " << sendcnt[1] << " " << sendcnt[2] << " " << sendcnt[3] << " " << endl;
+//		cout << "displ" << endl;
+//		cout << displ[0] << " " << displ[1] << " " << displ[2] << " " << displ[3] << " " << endl;
+//	}
 
-	MPI::COMM_WORLD.Scatterv(&(A[0][0]), sendcnt[0], displ[0], MPI_DOUBLE, &(recvA[0][0]), sendcnt[0][rank], MPI_DOUBLE, 0);
-	MPI::COMM_WORLD.Scatterv(&(B[0][0]), sendcnt[1], displ[1], MPI_DOUBLE, &(recvB[0][0]), sendcnt[1][rank], MPI_DOUBLE, 0);
+	recvA = alloc_2d_array_double(recv_dimensions[0][rank], recv_dimensions[1][1]);
+	recvB = alloc_2d_array_double(recv_dimensions[0][rank], recv_dimensions[1][1]);
 
+	MPI::COMM_WORLD.Scatterv(&(A[0][0]), sendcnt, displ, MPI_DOUBLE, &(recvA[0][0]), sendcnt[rank], MPI_DOUBLE, 0);
+	MPI::COMM_WORLD.Scatterv(&(B[0][0]), sendcnt, displ, MPI_DOUBLE, &(recvB[0][0]), sendcnt[rank], MPI_DOUBLE, 0);
+
+//	if(rank == 3){
+//		cout << "recvA" << endl;
+//		print_matrix_double(recvA,recv_dimensions[0][rank], recv_dimensions[1][1], 0);
+//		cout << "recvB" << endl;
+//		print_matrix_double(recvB,recv_dimensions[0][rank], recv_dimensions[1][1], 0);
+//	}
 	double **temp_result;
-	temp_result = alloc_2d_array_double(recv_dimensions[0][rank], recv_dimensions[2][0]);
-	i = rank;
+	temp_result = alloc_2d_array_double(recv_dimensions[0][rank], recv_dimensions[1][0]);
+	counter = rank;
 	do{
-		multiply_part(recvA, recvB, temp_result, recv_dimensions[0][rank], recv_dimensions[1][i], recv_dimensions[2][1], ((displ[1][i])/(recv_dimensions[2][1])), rank);
+		for(int i = 0; i < recv_dimensions[0][rank]; i++){
+			int offset = ((displ[counter])/(recv_dimensions[1][1]));
+			for(int j = 0; j < recv_dimensions[0][counter]; j++){
+				temp_result[i][offset] = 0;
+				for(int k = 0; k < recv_dimensions[1][1]; k++){
+					temp_result[i][j+offset] += recvA[i][k] * recvB[j][k];
+				}
+				//offset++;
+			}
+		}
 		if(rank == 0){
-			MPI::COMM_WORLD.Send(&(recvB[0][0]), sendcnt[1][i], MPI_DOUBLE, rank+1, 0);
+			MPI::COMM_WORLD.Send(&(recvB[0][0]), sendcnt[counter], MPI_DOUBLE, rank+1, 0);
 			free(recvB[0]);
 			free(recvB);
-			if(i == 0){
-				i = size;
+			if(counter == 0){
+				counter = size;
 			}
-			i--;
-			recvB = alloc_2d_array_double(recv_dimensions[1][i], recv_dimensions[2][1]);
-			MPI::COMM_WORLD.Recv(&(recvB[0][0]), sendcnt[1][i], MPI_DOUBLE, size-1, 0);
+			counter--;
+			recvB = alloc_2d_array_double(recv_dimensions[0][counter], recv_dimensions[1][1]);
+			MPI::COMM_WORLD.Recv(&(recvB[0][0]), sendcnt[counter], MPI_DOUBLE, size-1, 0);
 		}else {
 			double **tempB_part;
-			int temp_i;
-			tempB_part = alloc_2d_array_double(recv_dimensions[1][i], recv_dimensions[2][1]);
-			copy(&recvB[0][0], &recvB[0][0]+(recv_dimensions[1][i]*recv_dimensions[2][1]), &tempB_part[0][0]);
-			temp_i = i;
+			int temp_counter;
+			tempB_part = alloc_2d_array_double(recv_dimensions[0][counter], recv_dimensions[1][1]);
+			copy(&recvB[0][0], &recvB[0][0]+(recv_dimensions[0][counter]*recv_dimensions[1][1]), &tempB_part[0][0]);
+			temp_counter = counter;
 			free(recvB[0]);
 			free(recvB);
-			if(i == 0){
-				i = size;
+			if(counter == 0){
+				counter = size;
 			}
-			i--;
-			recvB = alloc_2d_array_double(recv_dimensions[1][i], recv_dimensions[2][1]);
-			MPI::COMM_WORLD.Recv(&(recvB[0][0]), sendcnt[1][i], MPI_DOUBLE, rank-1, 0);
+			counter--;
+			recvB = alloc_2d_array_double(recv_dimensions[0][counter], recv_dimensions[1][1]);
+			MPI::COMM_WORLD.Recv(&(recvB[0][0]), sendcnt[counter], MPI_DOUBLE, rank-1, 0);
 			if(rank == (size-1)){
-				MPI::COMM_WORLD.Send(&(tempB_part[0][0]), sendcnt[1][temp_i], MPI_DOUBLE, 0, 0);
+				MPI::COMM_WORLD.Send(&(tempB_part[0][0]), sendcnt[temp_counter], MPI_DOUBLE, 0, 0);
 			}else{
-				MPI::COMM_WORLD.Send(&(tempB_part[0][0]), sendcnt[1][temp_i], MPI_DOUBLE, rank+1, 0);
+				MPI::COMM_WORLD.Send(&(tempB_part[0][0]), sendcnt[temp_counter], MPI_DOUBLE, rank+1, 0);
 			}
 			free(tempB_part[0]);
 			free(tempB_part);
 		}
-	}while (i != rank);
+		if(rank == 1){
+			cout << "counter: " << counter << endl;
+			print_matrix_double(recvB,recv_dimensions[0][rank], recv_dimensions[1][1], 0);
+		}
+	}while (counter != rank);
 
-	MPI::COMM_WORLD.Gatherv(&(temp_result[0][0]), sendcnt[0][rank], MPI_DOUBLE, &result[0][0], sendcnt[0], displ[0], MPI_DOUBLE, 0);
+	if(rank == 0){
+		result = alloc_2d_array_double(recv_dimensions[1][0], recv_dimensions[1][0]);
+	}
+	MPI::COMM_WORLD.Gatherv(&(temp_result[0][0]), sendcnt[rank], MPI_DOUBLE, &result[0][0], sendcnt, displ, MPI_DOUBLE, 0);
 
 	if(rank == 0){
 		char filename[15] = {0};
 		sprintf(filename, "result.txt");
-		print_matrix_to_file(result, filename, recv_dimensions[2][0], recv_dimensions[2][0], 0);
+		print_matrix_to_file(result, filename, recv_dimensions[1][0], recv_dimensions[1][0], 0);
 	}
 
 	if(rank == 0){
@@ -278,14 +283,12 @@ int main(int argc, char *argv[]) {
 	free(recvB);
 	free(recv_dimensions[0]);
 	free(recv_dimensions);
-	free(sendcnt[0]);
 	free(sendcnt);
-	free(displ[0]);
 	free(displ);
 	free(temp_result[0]);
 	free(temp_result);
 
-	cout << "Process " << rank << " finished" << endl;
+	//cout << "Process " << rank << " finished" << endl;
 
 	MPI::Finalize();
 	return 0;
--- matrix_mult_openMP/src/matrix_mult_openMP.c
+++ matrix_mult_openMP/src/matrix_mult_openMP.c
@@ -1,10 +1,10 @@
 /*
  ============================================================================
  Name        : matrix_mult_openMP.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello OpenMP World in C
+ Author      : ck
+ Version     : 1.0
+ Copyright   :
+ Description : parallel multiplication of two random matrices
  ============================================================================
  */
 #include <omp.h>
@@ -12,11 +12,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+// generate random double between low and high
 double drand ( double low, double high )
 {
     return ( (double)rand() * ( high - low ) ) / (double)RAND_MAX + low;
 }
 
+//allocate memory to a two dimensional pointer
 double **alloc_2d_array_double(int rows, int cols) {
 	double *data = (double *)malloc(rows*cols*sizeof(double));
 	double **array= (double **)malloc(rows*sizeof(double*));
@@ -26,46 +28,67 @@ double **alloc_2d_array_double(int rows, int cols) {
 	return array;
 }
 
+//print a matrix to a file
+void print_matrix(double **matrix, char *filename, int rows, int cols){
+	FILE *fp;
+	fp = fopen (filename, "w+");
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			fprintf(fp, "%f ", matrix[i][j]);
+		}
+		fprintf(fp, "\n");
+	}
+	fclose(fp);
+}
+
 int main (int argc, char *argv[]) {
 
+	//initialize all important variables
 	int numThreads, tid, xdim, ydim, i, j, k;
 	double **A, **B, **result;
-	printf("Please put in the x dimension of the first matrix: ");
+
+	//request the dimensions of the matrices from the user
+	printf("Please put in the x dimension matrices: ");
 	scanf("%d", &xdim);
-	printf("Please put in the y dimension of the first matrix: ");
+	printf("Please put in the y dimension matrices: ");
 	scanf("%d", &ydim);
 
+	//allocate memory to the matrix pointers
 	A = alloc_2d_array_double(xdim, ydim);
 	B = alloc_2d_array_double(ydim, xdim);
 	result = alloc_2d_array_double(xdim, xdim);
 
-	FILE *fp_a, *fp_b;
-	fp_a = fopen ("A.txt", "w+");
-	fp_b = fopen ("B.txt", "w+");
+	//initialize matrices with random values
 	for(i = 0; i < xdim; i++){
 		for(j = 0; j < ydim; j++){
-			A[i][j] = drand(0, 10);
-			B[i][j] = drand(0, 10);
-			fprintf(fp_a, "%f ", A[i][j]);
-			fprintf(fp_b, "%f ", B[i][j]);
+			A[i][j] = drand(0.0, 10.0);
+			B[j][i] = drand(0.0, 10.0);
 		}
-		fprintf(fp_a, "\n");
-		fprintf(fp_b, "\n");
 	}
-	fclose(fp_a);
-	fclose(fp_b);
 
+//start multiple threads for computation
 #pragma omp parallel private(numThreads, tid, i, j, k) default(shared)
 {
 	int xportion, xrest, offset;
+
+	//get thread number and total number of threads
 	tid = omp_get_thread_num();
 	numThreads = omp_get_num_threads();
 
+	//calculate the portion of the computation, the thread will make
+	//and the offset at which the thread will start
 	xportion = xdim/(numThreads);
-	// rest verteilen
 	xrest = xdim%(numThreads);
-	offset = xportion*tid;
+	offset = 0;
+	//if the workload is not evenly dividable for all threads, spread the rest and adjust the offset
+	if(xrest > tid){
+		xportion++;
+	}else{
+		offset += xrest;
+	}
+	offset += xportion*tid;
 
+	//compute part_of_matrix_A * matrix_B
 	for(i = offset; i < (offset+xportion); i++){
 		for(j = 0; j < xdim; j++){
 			for(k = 0; k < ydim; k++){
@@ -73,25 +96,27 @@ int main (int argc, char *argv[]) {
 			}
 		}
 	}
-	printf("Thread: %d i: %d j: %d k: %d\n", tid,i,j,k);
-
 }
-	FILE *fp_res;
-	fp_res = fopen("result.txt", "w+");
-	for(i = 0; i < xdim; i++){
-		for(j = 0; j < xdim; j++){
-			fprintf(fp_res, "%f ", result[i][j]);
-		}
-		fprintf(fp_res, "\n");
-	}
-	fclose(fp_res);
+	//print all matrices to their respective files
+	char filename[20] = {0};
+	sprintf(filename, "A.txt");
+	print_matrix(A, filename, xdim, ydim);
+	sprintf(filename, "B.txt");
+	print_matrix(B, filename, ydim, xdim);
+	sprintf(filename, "result.txt");
+	print_matrix(result, filename, xdim, xdim);
 
+	//free the memory blocked by the matrices
 	free(A[0]);
 	free(A);
 	free(B[0]);
 	free(B);
 	free(result[0]);
 	free(result);
+
+	printf("Program Finished\n");
+	printf("Look up files A.txt and B.txt to see the matrixes used, and look up result.txt to see the result");
+
  return 0;
 }
 
